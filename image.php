16b5abaefedc31063ea9bcdf456aa9f96594a58b
ahutchinson11 <44821450+ahutchinson11@users.noreply.github.com>
2018-11-06 15:20:51 -0800

    Update image.php

--- image.php
+++ image.php
@@ -1,10 +1,10 @@
 <?php
-// Create a 55x30 image
+// Create a 55x30 image 
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
 $deployment = getenv("COLOR");
-
+//
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
